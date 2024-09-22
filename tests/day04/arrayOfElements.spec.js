2d885526d84701a2129d75f44248e18bb2cc4746
AB1974 <bekemaylin@gmail.com>
2024-09-22 08:24:32 -0700

    day04

---
+++ tests/day04/arrayOfElements.spec.js
@@ -0,0 +1,53 @@
+import { test,expect } from '@playwright/test';
+
+test.describe('Test Group', () => {
+let elements;
+    test.beforeEach(async({page})=>{
+      await page.goto("https://practice.cydeo.com/");
+      elements = await page.locator("//ul[@class='list-group']/li/a").all(); //returns array
+    });
+  test("Verify that there are exactly 50 link elements within the <ul> element.", async ({page,
+  }) => {
+  
+  expect(elements.length).toBe(50);
+  //another way :
+  expect(await page.locator("//ul[@class='list-group']/li").count()).toBe(50);
+
+  });
+
+  test("Verify that each of the 50 link elements within the <ul> elements are visible ", async ({page,
+  }) => {
+
+    for(let element of elements){
+     await   expect(element).toBeVisible();
+   //  expect(await element.isVisible()).toBeTruthy();
+    }
+
+  });
+
+  test("Verify that each of the 50 link elements within the <ul> elements are enable ", async ({page,
+  }) => {
+    for (let i = 0; i < elements.length; i++) {
+      await expect(elements[i]).toBeEnabled();
+      //   expect(await elements[i].isEnabled()).toBeTruthy;
+    }
+  });
+
+  test("erify that each of the 50 link elements within the <ul> element has a valid `href` attribute.", async ({page,
+  }) => {
+
+    for (let element of elements) {
+      let href = await element.getAttribute("href");
+      console.log(href);
+    //  expect(await element.isVisible()).toBeTruthy();
+    }
+
+    elements.forEach(async (element) => {
+      let href = await element.getAttribute("href");
+      expect(href).toBeTruthy();
+      console.log(href);
+    });
+
+  });
+});
+
