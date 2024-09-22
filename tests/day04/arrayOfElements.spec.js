8dcc296d4e039ebcd8e55c8ddd834a200ddef4a7
AB1974 <bekemaylin@gmail.com>
2024-09-22 10:55:02 -0700

    day4 2

--- tests/day04/arrayOfElements.spec.js
+++ tests/day04/arrayOfElements.spec.js
@@ -1,6 +1,6 @@
 import { test,expect } from '@playwright/test';
 
-test.describe('Test Group', () => {
+test.describe('Array of Elements', () => {
 let elements;
     test.beforeEach(async({page})=>{
       await page.goto("https://practice.cydeo.com/");
